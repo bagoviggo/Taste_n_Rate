52621b052fc0164be84419750b85ef02a51c7b81
bagoviggo <bagoviggo@gmail.com>
2024-02-26 00:35:53 +0300

    Refactored tests to accomodate all serializers

--- backend/reviews/tests.py
+++ backend/reviews/tests.py
@@ -1,22 +1,36 @@
 from django.test import TestCase
 from django.contrib.auth.models import User, Group
 from .serializers import UserSerializer, GroupSerializer
+from rest_framework.test import APIRequestFactory, force_authenticate
+
 
 class SerializerTest(TestCase):
     def setUp(self):
+        self.factory = APIRequestFactory()
+        
         self.group = Group.objects.create(name='Test Group')
-        self.user = User.objects.create_user(username='testuser', email='test@example.com', password='testpassword')
+        self.user = User.objects.create_user(
+            username='testuser',
+            email='test@example.com',
+            password='testpassword')
         self.user.groups.add(self.group)
+        
 
     def test_user_serializer(self):
-        serializer = UserSerializer(self.user)
+        request = self.factory.get('/users/')     # Setup the request context
+        force_authenticate(request, user=self.user)
+        
+        serializer = UserSerializer(self.user, context={'request': request})
         data = serializer.data
         self.assertEqual(data['username'], 'testuser')
         self.assertEqual(data['email'], 'test@example.com')
         self.assertEqual(len(data['groups']), 1)
-        self.assertEqual(data['groups'][0], 'Test Group')
+        self.assertEqual(data['groups'][0], 'http://testserver/groups/1/')
 
     def test_group_serializer(self):
-        serializer = GroupSerializer(self.group)
+        request = self.factory.get('/groups/')     # Setup the request context
+        force_authenticate(request, user=self.user)
+        
+        serializer = GroupSerializer(self.group, context={'request': request})
         data = serializer.data
         self.assertEqual(data['name'], 'Test Group')
