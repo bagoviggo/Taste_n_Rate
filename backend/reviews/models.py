f4744ff636950baaafe9e78d633e55af7c892daa
bagoviggo <bagoviggo@gmail.com>
2024-02-25 23:15:15 +0300

    [Backend] Added reviews' models

--- backend/reviews/models.py
+++ backend/reviews/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Review(models.Model):
+	title = models.CharField(max_length=255)
+	content = models.TextField()
+	stars = models.IntegerField()
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Category(models.Model):
+	name = models.CharField(max_length=255)
+	ordinal = models.IntegerField()
+
+# TODO add Business class
