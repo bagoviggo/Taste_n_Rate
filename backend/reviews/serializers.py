a1c4436b49efd1f2553152ce94c606f73df39381
bagoviggo <bagoviggo@gmail.com>
2024-02-26 01:27:05 +0300

    [Backend]Added more serializers

--- backend/reviews/serializers.py
+++ backend/reviews/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
+from reviews.models import Review, Category, Business
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -11,3 +12,19 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
 	class Meta:
 		model = Group
 		fields = ['url', 'name']
+
+class ReviewSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
+        
+class CategorySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class BusinessSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Business
+        fields = '__all__'
