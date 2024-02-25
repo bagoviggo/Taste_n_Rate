2725f1bd66c269ccbf3178aef938010e3cdabf14
bagoviggo <bagoviggo@gmail.com>
2024-02-25 23:32:42 +0300

    [Backend] Added some testcases for the serializers

--- backend/reviews/tests.py
+++ backend/reviews/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.contrib.auth.models import User, Group
+from .serializers import UserSerializer, GroupSerializer
 
-# Create your tests here.
+class SerializerTest(TestCase):
+    def setUp(self):
+        self.group = Group.objects.create(name='Test Group')
+        self.user = User.objects.create_user(username='testuser', email='test@example.com', password='testpassword')
+        self.user.groups.add(self.group)
+
+    def test_user_serializer(self):
+        serializer = UserSerializer(self.user)
+        data = serializer.data
+        self.assertEqual(data['username'], 'testuser')
+        self.assertEqual(data['email'], 'test@example.com')
+        self.assertEqual(len(data['groups']), 1)
+        self.assertEqual(data['groups'][0], 'Test Group')
+
+    def test_group_serializer(self):
+        serializer = GroupSerializer(self.group)
+        data = serializer.data
+        self.assertEqual(data['name'], 'Test Group')
